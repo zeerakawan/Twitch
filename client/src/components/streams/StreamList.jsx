1d135d5dafe7ba1262c6a674cbf1978475a8c940
Zeerak Awan <31988902+zeerakawan@users.noreply.github.com>
2022-07-03 19:40:07 -0400

    Fetching all the streams from DB and showing on the screen

--- client/src/components/streams/StreamList.jsx
+++ client/src/components/streams/StreamList.jsx
@@ -1,5 +1,37 @@
-const StreamList = () => {
-  return <div>Stream List</div>;
+import { useEffect } from "react";
+
+import { connect } from "react-redux";
+import { fetchStreams } from "../../redux/actions/actions";
+
+const StreamList = (props) => {
+  useEffect(() => {
+    props.fetchStreams();
+  }, []);
+
+  const renderList = () => {
+    return props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h2>Streams List</h2>
+      <div className="ui celled list">{renderList()}</div>
+    </div>
+  );
 };
 
-export default StreamList;
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
